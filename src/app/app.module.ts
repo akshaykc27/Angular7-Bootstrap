a347ead5d77f65949743067bf8218d272d567139
akshay <akshaykc27@gmail.com>
2019-07-15 19:02:50 +0530

    "bootstrap"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule.forRoot(),
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    TypeaheadModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatMenuModule,
+    MatCardModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
